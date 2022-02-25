719594e67e67cba0f5f5b61d6f95cbddda4c1d1c
TeoVincent <artur.wincenciak@gmail.com>
2022-02-25 20:44:42 +0100

    Remove title

--- src/DailyStatusCollector/Program.cs
+++ src/DailyStatusCollector/Program.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Linq;
-
-namespace DailyStatusCollector
+﻿namespace DailyStatusCollector
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nHello Daily Status Collector!\n");
-
             var storage = new Storage();
             var commitRepository = new CommitRepository(storage);
             var @interface = new Interface(commitRepository);
-            @interface.Convas(args);
+            @interface.Canvas(args);
         }
     }
 }
