4668c481e74791acf38170adc59238dc86f14ea9
TeoVincent <artur.wincenciak@gmail.com>
2022-02-25 19:46:37 +0100

    Get hard coded commits

---
+++ src/DailyStatusCollector/CommitRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DailyStatusCollector
+{
+    internal class CommitRepository
+    {
+        public IEnumerable<Commit> Get()
+        {
+            return new []
+            {
+                new Commit("Code review od PR-777 done."),
+                new Commit("Created PR-888")
+            };
+        }
+    }
+}
