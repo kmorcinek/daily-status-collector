d3f26e2e100d79ace0f1d6a3cac7669ad9d1bb1c
TeoVincent <artur.wincenciak@gmail.com>
2022-02-25 20:01:55 +0100

    Store commits in file

---
+++ src/DailyStatusCollector/Storage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DailyStatusCollector
+{
+    internal class Storage
+    {
+        public IEnumerable<string> ReadAllLines() =>
+            File.ReadAllLines("storage");
+    }
+}
